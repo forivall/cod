a66ba67e77289fa9470dc3eeb539dac7a1009170
Emily M Klassen <forivall@gmail.com>
2022-07-04 14:28:56 -0700

    feat: improve zsh plugin

--- cod.plugin.zsh
+++ cod.plugin.zsh
@@ -1,5 +1,10 @@
-local FOUND_RBENV=$+commands[cod]
+local FOUND_COMMAND=$+commands[cod]
 
-if [[ $FOUND_RBENV -eq 1 ]]; then
+if ! [[ $FOUND_COMMAND -eq 1 ]] && [[ -x ${0:A:h}/cod ]]; then
+  FOUND_COMMAND=1
+  alias cod="${0:A:h}/cod"
+fi
+
+if [[ $FOUND_COMMAND -eq 1 ]]; then
   source <(cod init $$ zsh)
 fi
